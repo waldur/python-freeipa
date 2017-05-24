374815346232e703597e1483750cb00d39d38329
Victor Mireyev <victor@opennodecloud.com>
2017-05-24 20:17:28 +0300

    [WAL-819] Implement FreeIPA JSON RPC client.

---
+++ setup.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+
+install_requires = [
+    'requests',
+]
+
+
+tests_requires = [
+    'responses',
+]
+
+
+setup(
+    name='python-freeipa',
+    version='0.1',
+    author='OpenNode Team',
+    author_email='info@opennodecloud.com',
+    url='http://nodeconductor.com',
+    description='Lightweight FreeIPA client',
+    long_description=open('README.rst').read(),
+    install_requires=install_requires,
+    extras_require={
+        'tests': tests_requires,
+    },
+    package_dir={'': 'src'},
+    packages=find_packages('src', exclude=['*.tests', '*.tests.*', 'tests.*', 'tests']),
+    test_suite='python_freeipa.tests.suite',
+    classifiers=(
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: System :: Systems Administration :: Authentication/Directory'
+    )
+)
