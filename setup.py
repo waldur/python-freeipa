12f7ac9b69aa23c4cb331c15699c1dbf42ac25aa
Jenkins <jenkins@opennodecloud.com>
2019-04-30 17:34:54 +0300

    Preparing new release: 0.2.3

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ tests_requires = [
 
 setup(
     name='python-freeipa',
-    version='0.2.2',
+    version='0.2.3',
     author='OpenNode Team',
     author_email='info@opennodecloud.com',
     url='http://waldur.com',
