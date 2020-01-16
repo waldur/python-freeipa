16c0e33b082323d572fe3764ca3166bf2cea2b60
schlitzer <stephan.schultchen@gmail.com>
2020-01-16 21:12:02 +0100

    fix some flake8 complaints

--- src/python_freeipa/__init__.py
+++ src/python_freeipa/__init__.py
@@ -4,8 +4,8 @@ from python_freeipa.client_legacy import ClientLegacy
 from python_freeipa.client_meta import ClientMeta
 
 __all__ = [
-    AuthenticatedSession,
-    Client,
-    ClientLegacy,
-    ClientMeta
+    'AuthenticatedSession',
+    'Client',
+    'ClientLegacy',
+    'ClientMeta'
 ]
